032f6ca7d86cfdb3310793d424a95d7bb3876caf
profhack47 <johnkudadjiekabutetteh@outlook.com>
2022-04-06 17:08:16 +0000

    refactored code

---
+++ script.js
@@ -0,0 +1,75 @@
+import {weatherApp} from "./weatherApp.js"
+
+let dataDump;
+let query = [];
+
+//First load
+window.addEventListener("load", () => {
+  getNavData();
+  waitForInfo();
+});
+
+//Open search
+weatherApp.changeLocation.addEventListener("click", () => {
+  weatherApp.openModal(weatherApp.searchModal);
+});
+
+//Use current location
+weatherApp.useCurrentLocation.addEventListener("click", () => {
+  weatherApp.closeModal(weatherApp.searchModal);
+  weatherApp.openModal(weatherApp.loadingOrErrorModal);
+  getNavData();
+  waitForInfo();
+});
+
+//Close search
+weatherApp.closeButton.addEventListener("click", () => {
+  weatherApp.closeModal(weatherApp.searchModal);
+});
+
+//Reload
+weatherApp.reload.addEventListener("click", () => {
+  weatherApp.addRemoveClass("reload-spin", weatherApp.reload, 5000);
+  weatherApp.request(weatherApp.formatQuery(query));
+});
+
+//Switch temp units
+weatherApp.optionTempUnit.addEventListener("click", () => {
+  weatherApp.changeTempUnit();
+});
+
+//Get lat and long
+function getNavData() {
+  if (navigator.geolocation.getCurrentPosition) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      query = [position.coords.latitude, position.coords.longitude];
+    });
+  }
+}
+
+//Change location
+function getText() {
+  query = document.querySelector("input").value;
+}
+
+function search() {
+  getText();
+  if(query == ""){
+    weatherApp.emptyQueryError.classList.remove("hide")
+  } else {
+    weatherApp.closeModal(weatherApp.searchModal);
+    weatherApp.openModal(weatherApp.loadingOrErrorModal);
+    weatherApp.request(weatherApp.formatQuery(query), dataDump);
+  }
+
+}
+
+function waitForInfo() {
+  setTimeout(() => {
+    if (query[0] !== undefined) {
+      weatherApp.request(weatherApp.formatQuery(query), dataDump);
+    } else {
+      weatherApp.showInternetError();
+    }
+  }, 20000);
+}
